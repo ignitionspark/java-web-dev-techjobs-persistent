46f1d852090d2da4c91253111454297ec3b555c9
ignitionspark <lbonfonte@gmail.com>
2020-08-28 18:56:06 -0400

    Finished Assignment

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,32 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location Field can not be blank.")
+    private String location;
 
+    @ManyToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<Job>();
+
+    public Employer() { }
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,25 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
 public class Skill extends AbstractEntity {
+    @NotBlank(message = "Description Field can not be blank.")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    private String Description;
+@ManyToMany(mappedBy = "skills")
+    public List<Job> jobs= new ArrayList<Job>();
+    public Skill() { }
 
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
 }
